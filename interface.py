7ac73fa403f13a4173458f0500d2f832dea7a231
pallesupriyaorg <130435937+pallesupriyaorg@users.noreply.github.com>
2023-10-19 16:14:44 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import json
-from agent import query_agent, create_agent
+from agent import query_agent,create_agent
 
 def decode_response(response: str) -> dict:
     """This function converts the string response from the model to a dictionary object.
