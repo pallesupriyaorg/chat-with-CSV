ad24cbefb930f3d658def7c0bacd89b0ba82ad43
pallesupriyaorg <130435937+pallesupriyaorg@users.noreply.github.com>
2023-10-20 11:06:39 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,14 +1,16 @@
-#agent.py
+# agent.py
 from langchain import OpenAI
 from langchain.agents import create_pandas_dataframe_agent
 import pandas as pd
-#setting up the api key
+
+# Setting up the api key
 import environ
 
 env = environ.Env()
 environ.Env.read_env()
 
 API_KEY = env("apikey")
+
 
 def create_agent(filename: str):
     """
@@ -20,14 +22,15 @@ def create_agent(filename: str):
     Returns:
         An agent that can access and use the LLM.
     """
-# Create an OpenAI object
-llm = OpenAI(openai_api_key=API_KEY)
 
-# Read the CSV file into a pandas dataframe
-df = pd.read_csv(filename)
+    # Create an OpenAI object.
+    llm = OpenAI(openai_api_key=API_KEY)
 
-# Create a pandas DataFrame agent
-df = pd.read_csv(filename)
+    # Read the CSV file into a Pandas DataFrame.
+    df = pd.read_csv(filename)
+
+    # Create a Pandas DataFrame agent.
+    return create_pandas_dataframe_agent(llm, df, verbose=False)
 
 #agent.py
 
