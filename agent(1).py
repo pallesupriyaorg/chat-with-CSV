375f907476fd74dde1f0e289773db6c6f5dfe446
pallesupriyaorg <130435937+pallesupriyaorg@users.noreply.github.com>
2023-10-26 10:45:27 +0530

    Update agent(1).py

--- agent(1).py
+++ agent(1).py
@@ -70,13 +70,13 @@ def write_response(response_dict: dict):
 
 
 st.title("Chat with your CSV")
-st.write("Please upload your CSV file below.")
+#st.write("Please upload your CSV file below.")
 #uploaded_file = st.file_uploader("Upload a CSV")
 uploaded_file = st.sidebar.file_uploader("upload a file",type=['csv'])
 
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
-  st.write(df.head(3))
+  st.write(df.head(5))
   
   prompt = st.text_area("Enter your prompt:")
   
